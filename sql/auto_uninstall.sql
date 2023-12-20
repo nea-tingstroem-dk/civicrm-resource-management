f8923f18458a41537cf6c0e563814caa6ab90fa1
nea-tingstroem-dk <55084026+nea-tingstroem-dk@users.noreply.github.com>
2023-12-21 00:41:44 +0100

    Fix misspelling and do not dump tables at uninstall

--- sql/auto_uninstall.sql
+++ sql/auto_uninstall.sql
@@ -12,13 +12,13 @@
 -- *
 -- * Clean up the existing tables-- *
 -- *******************************************************/
-
-SET FOREIGN_KEY_CHECKS=0;
-
-DROP TABLE IF EXISTS `civicrm_resource_configuration`;
-DROP TABLE IF EXISTS `civicrm_resource_calendar_settings`;
-DROP TABLE IF EXISTS `civicrm_resource_calendar_participant`;
-DROP TABLE IF EXISTS `civicrm_resource_calendar_color`;
-DROP TABLE IF EXISTS `civicrm_resource_calendar`;
-
-SET FOREIGN_KEY_CHECKS=1;
+-- 
+-- SET FOREIGN_KEY_CHECKS=0;
+-- 
+-- DROP TABLE IF EXISTS `civicrm_resource_configuration`;
+-- DROP TABLE IF EXISTS `civicrm_resource_calendar_settings`;
+-- DROP TABLE IF EXISTS `civicrm_resource_calendar_participant`;
+-- DROP TABLE IF EXISTS `civicrm_resource_calendar_color`;
+-- DROP TABLE IF EXISTS `civicrm_resource_calendar`;
+-- 
+-- SET FOREIGN_KEY_CHECKS=1;
