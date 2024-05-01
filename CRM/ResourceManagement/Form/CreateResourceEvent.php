64ff0c85233f2f1991a425bcdb95773a7230d522
nea-tingstroem-dk <55084026+nea-tingstroem-dk@users.noreply.github.com>
2024-05-01 10:54:07 +0200

    Clone events feature

--- CRM/ResourceManagement/Form/CreateResourceEvent.php
+++ CRM/ResourceManagement/Form/CreateResourceEvent.php
@@ -158,7 +158,9 @@ class CRM_ResourceManagement_Form_CreateResourceEvent extends CRM_Core_Form {
     }
 
     $this->add('hidden', 'host_role_id', $this->_calendarSettings['host_role_id']);
-    $this->add('hidden', 'common_templates', $this->_calendarSettings['common_templates']);
+    if (isset($this->_calendarSettings['common_templates'])) {
+      $this->add('hidden', 'common_templates', $this->_calendarSettings['common_templates']);
+    }
     $filter = $this->_filter;
     if ($filter) {
       $this->add('hidden', 'min_start', $resources[$filter]['min_start']);
@@ -616,7 +618,8 @@ class CRM_ResourceManagement_Form_CreateResourceEvent extends CRM_Core_Form {
 
       if (substr_compare($buttonName, 'advanced', -8) === 0 ||
         substr_compare($buttonName, 'expand', -6) === 0) {
-        CRM_Utils_JSON::output(['openpage' => 'civicrm/a/#/resource/manage-event?event_id=' . $event->id]);
+        CRM_Utils_JSON::output(['openpage' => "civicrm/a/#/resource/manage-event?" .
+          "event_id={$event->id}&calendar_id={$this->_calendar_id}"]);
       } else if (substr_compare($buttonName, 'edit_event', -10) === 0) {
         CRM_Utils_JSON::output(['openpage' => 'civicrm/event/manage/settings?' .
           'reset=1&action=update&id=' . $this->_eventId]);
